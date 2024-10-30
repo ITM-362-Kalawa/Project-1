8e1ff4343082ecbc250fa7395764a5fd6a87a232
Ckalawa <145289597+Ckalawa@users.noreply.github.com>
2024-10-30 14:43:22 -0500

    Updated work
    Added all the work I should have done when I turned in the first deliverable

--- js/site.js
+++ js/site.js
@@ -1,5 +1,39 @@
-// jQuery 3.x-style ready event and locally scoped $
-jQuery(function($) {
-  $('html').removeClass('nojs');
-  $('html').addClass('hasjs');
+document.getElementById('submit').addEventListener('click', function(event) {
+  const email = document.getElementById('email').value;
+  const birthday = document.getElementById('birthday').value;
+  const ssn = document.getElementById('ssn').value;
+  const address = document.getElementById('address').value;
+  const ageVerified = document.getElementById('age-verification').checked;
+
+  if (!email) {
+    alert('Please enter your email.');
+    event.preventDefault();
+    return;
+  }
+
+  if (!birthday) {
+    alert('Please enter your birthday.');
+    event.preventDefault();
+    return;
+  }
+
+  if (!ssn.match(/^\d{3}-\d{2}-\d{4}$/)) {
+    alert('Please enter a valid SSN (format: 123-45-6789).');
+    event.preventDefault();
+    return;
+  }
+
+  if (!address) {
+    alert('Please enter your address.');
+    event.preventDefault();
+    return;
+  }
+
+  if (!ageVerified) {
+    alert('Please confirm that you are over 18.');
+    event.preventDefault();
+    return;
+  }
+
+  alert('Thanks you for your data Sucker!');
 });
